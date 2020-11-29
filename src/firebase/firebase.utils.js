5e6fc57b45e9d17bb7149f0d1cac616bb4905d37
Tuomas Martikainen <tuomasomartikainen@gmail.com>
2020-11-29 12:33:25 +0200

    End of lesson 102. implemented firebase utils, included ability to store authenticated users into firestore database.

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,34 @@ const config = {
     appId: "1:374504417927:web:fb20f47f8a2f88f79f2622"
   };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if(!userAuth) return;
+
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  
+  const snapShot  = await userRef.get();
+
+  if(!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
